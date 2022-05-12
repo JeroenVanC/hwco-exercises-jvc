661846462094ba3a3d8a01927e8b72e2468a1537
Jeroen Van Caekenberghe <jeroen.vc1@icloud.com>
2022-05-12 09:22:03 +0200

    t

--- exercise4Xo/compile-files/firmware/src/Xoodyak.c
+++ exercise4Xo/compile-files/firmware/src/Xoodyak.c
@@ -182,7 +182,11 @@ void Xoodoo_Permute(xoodyak_state *state){
     // }
 
     print_str("ST:");
-    print_hex(stateAsWords[10], 8);
+
+    for (int i = 0; i < 12; i++){
+        print_hex(stateAsWords[x], 8);
+    }
+    
     roundHWSW_reg(stateAsWords, RC[0]);
 
     Xoodoo_WordsToState(stateAsWords, state);
