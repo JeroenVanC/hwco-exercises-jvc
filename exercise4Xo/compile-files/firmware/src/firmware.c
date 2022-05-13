10dde45f3037d41e16bff6dc6ee2e5e8652f1e23
Jeroen Van Caekenberghe <jeroen.vc1@icloud.com>
2022-05-13 14:40:10 +0200

    test

--- exercise4Xo/compile-files/firmware/src/firmware.c
+++ exercise4Xo/compile-files/firmware/src/firmware.c
@@ -2,14 +2,14 @@
 #include "Xoodyak.h"
 #include <stddef.h>
 
-#define MESSAGE_LENGTH 3
+#define MESSAGE_LENGTH 5
 #define XOODYAK_DIGEST_SIZE 32
 
 void main(void){
 
     struct cyclist cy;
     //unsigned char message[MESSAGE_LENGTH] = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F\x20\x21\x22\x23\x24\x25\x26\x27";
-    unsigned char message[MESSAGE_LENGTH] = "\x00\x01\x02";
+    unsigned char message[MESSAGE_LENGTH] = "\x00\x01\x02\x03\x04";
     unsigned char digest[XOODYAK_DIGEST_SIZE];
 
     // HASHING
