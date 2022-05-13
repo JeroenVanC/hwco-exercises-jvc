9e3ceb261d0aa2a37b5247cced245d6cf3b170d6
Jeroen Van Caekenberghe <jeroen.vc1@icloud.com>
2022-05-13 14:09:31 +0200

    test

--- exercise4Xo/compile-files/firmware/src/XodHwCoupeling.c
+++ exercise4Xo/compile-files/firmware/src/XodHwCoupeling.c
@@ -21,9 +21,25 @@ void roundHWSW_reg(uint32_t *stateAsWords, uint32_t rc)
     //STATE_REG_in_11 = 0xFFFFFFFF;
 
     // 12de register 1 zetten | control-register
-    
-    //test
     ENABLE_CTRL = 0x00000001U;
+
+    // kijken wanneer de 12 rondes klaar zijn
+    while (!(STATE_RND_FINISHED == 0x00000001)) {
+        // HW blijft bezig tot 12de ronde
+    }
+
+    stateAsWords[0] = STATE_REG_out_0;
+    stateAsWords[1] = STATE_REG_out_1;
+    stateAsWords[2] = STATE_REG_out_2; 
+    stateAsWords[3] = STATE_REG_out_3; 
+    stateAsWords[4] = STATE_REG_out_4; 
+    stateAsWords[5] = STATE_REG_out_5; 
+    stateAsWords[6] = STATE_REG_out_6; 
+    stateAsWords[7] = STATE_REG_out_7; 
+    stateAsWords[8] = STATE_REG_out_8; 
+    stateAsWords[9] = STATE_REG_out_9; 
+    stateAsWords[10] = STATE_REG_out_10; 
+    stateAsWords[11] = STATE_REG_out_11; 
 
 }
 
--- exercise4Xo/compile-files/firmware/src/XodHwCoupeling.h
+++ exercise4Xo/compile-files/firmware/src/XodHwCoupeling.h
@@ -20,17 +20,20 @@
 
 #define  ENABLE_CTRL (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 12 * 4))
 
-#define  STATE_REG_out_1 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 13 * 4))
-#define  STATE_REG_out_2 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 14 * 4))
-#define  STATE_REG_out_3 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 15 * 4))
-#define  STATE_REG_out_4 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 16 * 4))
-#define  STATE_REG_out_5 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 17 * 4))
-#define  STATE_REG_out_6 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 18 * 4))
-#define  STATE_REG_out_7 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 19 * 4))
-#define  STATE_REG_out_8 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 20 * 4))
-#define  STATE_REG_out_9 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 21 * 4))
-#define  STATE_REG_out_10 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 22 * 4))
-#define  STATE_REG_out_11 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 23 * 4))
+#define  STATE_REG_out_0 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 13 * 4))
+#define  STATE_REG_out_1 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 14 * 4))
+#define  STATE_REG_out_2 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 15 * 4))
+#define  STATE_REG_out_3 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 16 * 4))
+#define  STATE_REG_out_4 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 17 * 4))
+#define  STATE_REG_out_5 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 18 * 4))
+#define  STATE_REG_out_6 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 19 * 4))
+#define  STATE_REG_out_7 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 20 * 4))
+#define  STATE_REG_out_8 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 21 * 4))
+#define  STATE_REG_out_9 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 22 * 4))
+#define  STATE_REG_out_10 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 23 * 4))
+#define  STATE_REG_out_11 (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 24 * 4))
+
+#define  STATE_RND_FINISHED (*(volatile unsigned int *)(XOODYAK_BASE_ADDR + 25 * 4))
 
 
 void roundHWSW_reg(uint32_t *stateAsWords, uint32_t rc);
